f14fb1751f327f56d96e06e90efc0aeaa0d7f844
fiosx-india <sharvikaenterprises2015@gmail.com>
2026-08-18 21:47:14 +0530

    Add files via upload

--- timeframe/registry.py
+++ timeframe/registry.py
@@ -1,1 +1,8 @@
-
+from ..constants import TIMEFRAMES
+NATIVE_INTERVALS={"1m","5m","15m","30m","45m","1h","1d","1wk","1mo"}
+def normalize_timeframe(tf:str)->str:
+    x=tf.strip().upper()
+    if x in {"60M","60MIN","1HR"}: return "1H"
+    return x
+def is_supported(tf:str)->bool:
+    return normalize_timeframe(tf) in TIMEFRAMES
